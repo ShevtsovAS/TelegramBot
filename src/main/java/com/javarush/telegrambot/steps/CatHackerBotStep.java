36b91830b05907ed75ae974710d3c843b4eae83c
Aleksandr Shevtsov <hack85sh@gmail.com>
2024-04-26 02:53:29 -0400

    Refactoring - use Enum ordinal instead of hardcoded stepNum

--- src/main/java/com/javarush/telegrambot/steps/CatHackerBotStep.java
+++ src/main/java/com/javarush/telegrambot/steps/CatHackerBotStep.java
@@ -15,17 +15,16 @@ import static java.util.stream.Collectors.toMap;
 
 @RequiredArgsConstructor
 public enum CatHackerBotStep {
-    START(0, 0, "step_1_pic", STEP_1_TEXT, STEP_1_BUTTONS, START_CONDITION),
-    STEP_1(1, 20, "step_2_pic", STEP_2_TEXT, STEP_2_BUTTONS, STEP_1_CONDITION),
-    STEP_2(2, 20, "step_3_pic", STEP_3_TEXT, STEP_3_BUTTONS, STEP_2_CONDITION),
-    STEP_3(3, 30, "step_4_pic", STEP_4_TEXT, STEP_4_BUTTONS, STEP_3_CONDITION),
-    STEP_4(4, 30, "step_5_pic", STEP_5_TEXT, STEP_5_BUTTONS, STEP_4_CONDITION),
-    STEP_5(5, 40, "step_6_pic", STEP_6_TEXT, STEP_6_BUTTONS, STEP_5_CONDITION),
-    STEP_6(6, 40, "step_7_pic", STEP_7_TEXT, STEP_7_BUTTONS, STEP_6_CONDITION),
-    STEP_7(7, 50, "step_8_pic", STEP_8_TEXT, STEP_8_BUTTONS, STEP_7_CONDITION),
-    STEP_8(8, 0, "final_pic", FINAL_TEXT, null, STEP_8_CONDITION);
+    START(0, "step_1_pic", STEP_1_TEXT, STEP_1_BUTTONS, START_CONDITION),
+    STEP_1(20, "step_2_pic", STEP_2_TEXT, STEP_2_BUTTONS, STEP_1_CONDITION),
+    STEP_2(20, "step_3_pic", STEP_3_TEXT, STEP_3_BUTTONS, STEP_2_CONDITION),
+    STEP_3(30, "step_4_pic", STEP_4_TEXT, STEP_4_BUTTONS, STEP_3_CONDITION),
+    STEP_4(30, "step_5_pic", STEP_5_TEXT, STEP_5_BUTTONS, STEP_4_CONDITION),
+    STEP_5(40, "step_6_pic", STEP_6_TEXT, STEP_6_BUTTONS, STEP_5_CONDITION),
+    STEP_6(40, "step_7_pic", STEP_7_TEXT, STEP_7_BUTTONS, STEP_6_CONDITION),
+    STEP_7(50, "step_8_pic", STEP_8_TEXT, STEP_8_BUTTONS, STEP_7_CONDITION),
+    STEP_8(0, "final_pic", FINAL_TEXT, null, STEP_8_CONDITION);
 
-    private final int stepNum;
     private final int glory;
     private final String photoKey;
     private final String text;
@@ -33,7 +32,7 @@ public enum CatHackerBotStep {
     private final Predicate<CatHackerBot> condition;
 
     public static final Map<Integer, CatHackerBotStep> VALUES_MAP = Arrays.stream(values())
-            .collect(toMap(it -> it.stepNum, it -> it));
+            .collect(toMap(Enum::ordinal, it -> it));
 
     public static Optional<CatHackerBotStep> getCurrentStep(CatHackerBot bot) {
         return Optional.ofNullable(VALUES_MAP.get(bot.getStep()));
